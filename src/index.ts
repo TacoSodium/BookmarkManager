af05fcc7d322c2d60e1d54fbb5f9aee9b011342b
Coryn Tyrrell <103038198@student.swin.edu.au>
2020-05-28 11:25:01 +1000

    class begun

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello");
+class Bookmark {
+    Title: string;
+    Icon: string;
+    Favourite: boolean;
+    constructor(title: string, icon: string, favourite: boolean) {
+        this.Title = title;
+        this.Icon = icon;
+        this.Favourite = favourite;
+    }
+    SetURL(url: string) {
+
+    }
+    GetURL() {
+        
+    }
+}
