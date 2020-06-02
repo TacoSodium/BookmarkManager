fada1e85ce60c2e9eeb28e5ffeaa73698c74bb4e
Coryn Tyrrell <103038198@student.swin.edu.au>
2020-06-02 11:00:23 +1000

    debugging

--- dist/index.js
+++ dist/index.js
@@ -3,21 +3,23 @@ let bookmarkContainer = document.getElementById("bookmarkContainer");
 let addBookmarkButton = document.getElementById("addBookmarkButton");
 addBookmarkButton.addEventListener('click', () => {
     //take user input
-    console.log("test test");
     let titleInput = document.getElementById("titleInput");
     let bookmarkTitle = document.createTextNode(titleInput.value);
     let urlInput = document.getElementById("urlInput");
     let bookmarkURL = document.createTextNode(urlInput.value);
-    let newBookmarkDetails = new BookmarkDetails(bookmarkTitle, bookmarkURL);
-    let newBookmark = new Bookmark(newBookmarkDetails);
-    //creates bookmark
-    if (addBookmarkButton !== null) {
-        newBookmark.Spawn(bookmarkContainer);
+    if (bookmarkURL.nodeValue == "") {
+        alert("Your bookmark has no address");
     }
-    // removes bookmark
-    newBookmark.FavouriteButton.addEventListener('click', () => {
-        if (newBookmark !== null) {
-            newBookmark.Unfavourite();
-        }
-    });
+    else {
+        let newBookmarkDetails = new BookmarkDetails(bookmarkTitle, bookmarkURL);
+        let newBookmark = new Bookmark(newBookmarkDetails);
+        //creates bookmark
+        newBookmark.Spawn(bookmarkContainer);
+        // removes bookmark
+        newBookmark.FavouriteButton.addEventListener('click', () => {
+            if (newBookmark !== null) {
+                newBookmark.Unfavourite();
+            }
+        });
+    }
 });
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,29 @@
-import {Bookmark, BookmarkDetails} from "./Bookmark.js";
+import { Bookmark, BookmarkDetails } from "./Bookmark.js";
 
 let bookmarkContainer = document.getElementById("bookmarkContainer") as HTMLDivElement;
 let addBookmarkButton = document.getElementById("addBookmarkButton") as HTMLButtonElement;
 
 addBookmarkButton.addEventListener('click', () => {
     //take user input
-    console.log("test test");
-
     let titleInput = document.getElementById("titleInput") as HTMLInputElement;
     let bookmarkTitle = document.createTextNode(titleInput.value) as Node;
     let urlInput = document.getElementById("urlInput") as HTMLInputElement;
     let bookmarkURL = document.createTextNode(urlInput.value) as Node;
 
-    let newBookmarkDetails = new BookmarkDetails(bookmarkTitle, bookmarkURL);
-    let newBookmark = new Bookmark(newBookmarkDetails);
+    if (bookmarkURL.nodeValue == "") {
+        alert("Your bookmark has no address");
+    } else {
+        let newBookmarkDetails = new BookmarkDetails(bookmarkTitle, bookmarkURL);
+        let newBookmark = new Bookmark(newBookmarkDetails);
 
-    //creates bookmark
-    if (addBookmarkButton !== null) {
+        //creates bookmark
         newBookmark.Spawn(bookmarkContainer);
-    }
 
-    // removes bookmark
-    newBookmark.FavouriteButton.addEventListener('click', () => {
-        if (newBookmark !== null) {
-            newBookmark.Unfavourite();
-        }
-    })
+        // removes bookmark
+        newBookmark.FavouriteButton.addEventListener('click', () => {
+            if (newBookmark !== null) {
+                newBookmark.Unfavourite();
+            }
+        })
+    }
 })
